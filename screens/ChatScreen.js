ae44c959809c6cd0330bbb9567e4a603e94e9f63
milton <milton@miltons-MacBook.local>
2020-12-18 12:13:52 +0800

    add chat ability

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,25 +1,71 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import {
   View,
   Text,
   TextInput,
   StyleSheet,
   TouchableOpacity,
+  Button,
 } from "react-native";
-
+import firebase from "../database/firebaseDB";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { GiftedChat } from "react-native-gifted-chat";
+const auth = firebase.auth();
 export default function ChatScreen({ navigation }) {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.navigate("Chat");
+      } else {
+        navigation.navigate("Login");
+      }
+    });
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={logout}>
+          <MaterialCommunityIcons
+            name="logout"
+            size={20}
+            color="black"
+            style={{ marginRight: 20 }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 288888888888888,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+    return unsubscribe;
+  }, []);
+  function logout() {
+    auth.signOut();
+  }
+  function onSend(newMessages) {
+    setMessages([...newMessages, ...messages]);
+  }
   return (
-    <View>
-      <Text>Hello this is chat</Text>
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={(newMessages) => onSend(newMessages)}
+      renderUsernameOnMessage={true}
+      listViewProps={{
+        style: {
+          backgroundColor: "#666",
+        },
+      }}
+      user={{
+        _id: 1,
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
